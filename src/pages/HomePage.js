ad9f1e2b8c8d96b1e1aa36746fca19dc02eb8ebd
Jared Long <jlong4223@gmail.com>
2021-01-22 11:00:00 -0600

    sets up data on the cards

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,47 @@
 import styled from "styled-components";
+import { fetchStarships } from "../services/starwars-api";
 
 const StyledPage = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-wrap: wrap;
+  @media (max-width: 1150px) {
+    flex-direction: column;
+  }
+`;
+
+const StyledStarshipCard = styled.article`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  margin: 15px;
+  border: 1px solid #000;
+  border-radius: 12px;
+  height: 250px;
+  width: 350px;
+  h1 {
+    text-align: center;
+  }
 `;
 
 export default function HomePage(props) {
   return (
     <StyledPage>
-      <h1>HomePage</h1>
+      {props.sampleStarships.map((starship) => (
+        <StyledStarshipCard key={starship.name}>
+          <h1>{starship.name}</h1>
+          <p>
+            <strong>Manufacturer:</strong> {starship.manufacturer}
+          </p>
+          <p>
+            <strong>Passenger Capactity:</strong> {starship.passengers}
+          </p>
+          <p>
+            <strong>Hyperdrive:</strong> {starship.hyperdrive_rating}
+          </p>
+        </StyledStarshipCard>
+      ))}
     </StyledPage>
   );
 }
